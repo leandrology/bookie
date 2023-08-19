a596bc29bb486973cacc67b9d0d7f18b045dbdf5
leandrology <leandrology@gmail.com>
2023-08-19 16:49:41 +0800

    Added whole project

---
+++ src/helpers/constants/book-data.ts
@@ -0,0 +1,27 @@
+export const bookData = `
+<url>
+<loc>https://books.toscrape.com/</loc>
+<desc>Your book store</desc>
+</url>
+<url>
+<loc>https://books.toscrape.com/catalogue/category/books_1/index.html</loc>
+<desc>An overview about our book collection</desc>
+</url>
+<url>
+<loc>https://books.toscrape.com/catalogue/category/books/childrens_11/page-1.html</loc>
+<content>
+    <book>
+    <url>https://books.toscrape.com/catalogue/the-bear-and-the-piano_967/index.html</url>
+    <desc>The Bear and the Piano</desc>
+    <price>£36.89</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>https://books.toscrape.com/catalogue/twenty-yawns_773/index.html</url>
+    <desc>Twenty Yawns/desc>
+    <price>£22.08</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>
+`
